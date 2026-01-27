878650dc16ca42c4d3e77681dd0766afa2da97a7
Arej02 <khadkaarya20@outlook.com>
2026-01-27 15:51:16 +1030

    feat(api): added the tables and schema endpoints

---
+++ src/api/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from sqlalchemy import create_engine,inspect
+from dotenv import load_dotenv,find_dotenv
+import os
+
+# Setting up the environment variables:
+load_dotenv(find_dotenv())
+key=os.getenv("DATABASE_URL")
+
+if not key:
+    raise ValueError("Datbase doesnot exist")
+print("Successfully connected")
+
+# Intializing the inspect object:
+engine=create_engine(key)
+insp=inspect(engine)
+
+app=FastAPI()
+
+# Home Endpoint:
+@app.get("/")
+def home():
+    return {"message":"Welcome to Text to SQL Agent"}
+
+# Get table names in the database:
+@app.get("/tables")
+def get_table_names():
+    tables=insp.get_table_names()
+    return {
+        "table": tables
+    }
+
+# Get the feature name of each table
+@app.get("/schemas")
+def get_schema():
+    new_dict={}
+    for col in insp.get_table_names():
+        new_list=[]
+        for val in insp.get_columns(col):
+            features=val.get("name","")
+            new_list.append(features)
+
+        new_dict[col]=new_list
+    return new_dict
+
+
+
+
+    
