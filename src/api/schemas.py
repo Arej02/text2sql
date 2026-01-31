0cc594c7c449d200a979372afe1f3887f236f1b7
Arej02 <khadkaarya20@outlook.com>
2026-01-31 20:10:50 +1030

    fix: fixed an issue

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -3,8 +3,13 @@ from typing import Annotated
 
 class InputSchema(BaseModel):
     question:Annotated[str,Field(...,description="Enter your question here based on you database",example="Who got the highest marks")]
+    thread_id:str="default"
 
 class OutputSchema(BaseModel):
+    question:str
     sql:Annotated[str,Field(...,description="SQL query as per the question")]
     confidence_score:Annotated[float,Field(...,description="Confidence score in a sclae of 0 to 1",ge=0,le=1)]
     feedback:Annotated[str,Field(...,description="Feedback in a line about the query or if the question was not understood")]
+    rows_count:int
+    rows:list[dict]
+    messages:list[dict]
